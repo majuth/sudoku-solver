1f3ec7c86c2e788daca6a1ba55be313c9d38ac09
Majuth <majuth101@hotmail.com>
2021-06-09 18:05:03 -0400

    Creating checking functions

---
+++ solver.py
@@ -0,0 +1,20 @@
+def numInRow(arr, row, num):
+    for col in range(9):
+        if (arr[row][col]==num):
+            return True
+    return False
+
+def numInCol(arr, col, num):
+    for row in range(9):
+        if (arr[row][col]==num):
+            return True
+    return False
+
+def numInBox(arr, row, col, num):
+    startRowBox = row - (row % 3)
+    startColBox = col - (col % 3)
+    for currRow in range(startRowBox, startColBox+3):
+        for currCol in range(startColBox, startColBox+3):
+            if (arr[currRow][currCol]==num):
+                return True
+    return False
